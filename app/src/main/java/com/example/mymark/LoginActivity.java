c759cc14979893336f207eaac955471018166450
ayushgharat <gharatayush27@gmail.com>
2022-10-22 04:43:29 -0400

    First PR to Main
    Login page in construction

--- app/src/main/java/com/example/mymark/LoginActivity.java
+++ app/src/main/java/com/example/mymark/LoginActivity.java
@@ -3,6 +3,10 @@ package com.example.mymark;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        EditText et1 = (EditText) findViewById(R.id.et_email_login);
+        Button bt = (Button) findViewById(R.id.bt_login);
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(LoginActivity.this, et1.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
