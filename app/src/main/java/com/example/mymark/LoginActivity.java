8648fd6e578def60a6733fea29d0e7beb9759fb5
ayushgharat <gharatayush27@gmail.com>
2022-10-22 16:15:32 -0400

    Firebase Login Integration

--- app/src/main/java/com/example/mymark/LoginActivity.java
+++ app/src/main/java/com/example/mymark/LoginActivity.java
@@ -1,29 +1,67 @@
 package com.example.mymark;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText et1, et2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        EditText et1 = (EditText) findViewById(R.id.et_email_login);
+        et1 = (EditText) findViewById(R.id.et_email_login);
+        et2 = (EditText) findViewById(R.id.et_password_login);
         Button bt = (Button) findViewById(R.id.bt_login);
+
+        mAuth = FirebaseAuth.getInstance();
 
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(LoginActivity.this, et1.getText().toString(), Toast.LENGTH_SHORT).show();
+                //Toast.makeText(LoginActivity.this, et1.getText().toString(), Toast.LENGTH_SHORT).show();
+                AllowUserToLogin();
             }
         });
+    }
 
+    private void AllowUserToLogin() {
+        String email = et1.getText().toString();
+        String password = et2.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Please Enter Email Address", Toast.LENGTH_SHORT).show();
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please Enter Password", Toast.LENGTH_SHORT).show();
+        } else {
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        //SendUserToMainActivity();
+                        Toast.makeText(LoginActivity.this, "Log in successful", Toast.LENGTH_SHORT).show();
+                    } else {
+                        String message = task.getException().toString();
+                        Toast.makeText(LoginActivity.this, "Error : " + message, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 }
